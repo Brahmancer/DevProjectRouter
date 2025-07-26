24c113584626ba04500249745b57a6b4fd5a5257
dmiller <dmiller4991@gmail.com>
2025-07-26 11:25:36 -0400

    Initial project creation.

---
+++ DevProjectServer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DevProjectServer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
